6cd27ec1eee0db19643c64394c41487a7b54ac12
Harshit Gupta <kohthecodemaster@gmail.com>
2024-03-03 23:32:09 +0530

    23rd Commit - LeetCode #: 49. Group Anagrams - Using HashMap with Character Frequency as Key
    New Topics Learnt:
    - Crafting a custom key from character frequencies for better data organization.
    
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S49.java
+++ Java Playground/src/practice/leetcode/S49.java
@@ -11,7 +11,9 @@ public class S49 {
 
     private void major() {
 
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+//        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+//        String[] strs = {"eat", "tan"};
+        String[] strs = {"bdddddddddd", "bbbbbbbbbbc"};
 //        String[] strs = {"abc", "bca", "dbc"};
 
         List<List<String>> lists = groupAnagrams(strs);
@@ -21,7 +23,9 @@ public class S49 {
 
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        return solutionUsingHashMap(strs);
+//        return solutionUsingHashMap(strs);
+
+        return solutionUsingHashMapWithCharFreqAsKey(strs);
 
 //        return solutionUsingCharFreqArr(strs);
 
@@ -65,6 +69,60 @@ public class S49 {
         for (Character c : word.toCharArray()) charFreqMap.put(c, charFreqMap.getOrDefault(c, 0) + 1);
 
         return charFreqMap;
+
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    private List<List<String>> solutionUsingHashMapWithCharFreqAsKey(String[] strs) {
+        /*
+            Time Complexity:    O(n * m)
+            Space Complexity:   O(n * m)
+            Approach:           Using HashMap
+            Note:               n is the number of words and m is the average length of the words.
+        */
+
+        List<List<String>> groupedAnagrams;
+
+        //  Store CharFreqMap as Key & List of words that has the same CharFreqMap as Value
+        Map<String, List<String>> strKeyCharFreqToWords = new HashMap<>();
+
+        for (String currentWord : strs) {
+            String currentWordStrKeyCharFreq = generateStrKeyCharFreq(currentWord);
+
+            List<String> wordsWithSameStrKeyCharFreq = strKeyCharFreqToWords.getOrDefault(currentWordStrKeyCharFreq, new ArrayList<>());
+            wordsWithSameStrKeyCharFreq.add(currentWord);
+
+            strKeyCharFreqToWords.put(currentWordStrKeyCharFreq, wordsWithSameStrKeyCharFreq);
+        }
+
+        groupedAnagrams = new ArrayList<>(strKeyCharFreqToWords.values());
+        return groupedAnagrams;
+
+    }
+
+    private String generateStrKeyCharFreq(String word) {
+
+        StringBuilder strKeyCharFreq = new StringBuilder();
+        int[] arrCharFreq = new int[26];
+
+        for (Character c : word.toCharArray()) arrCharFreq[c - 'a']++;
+
+        //  Store Char Freq Pattern in the form of String | E.g.: AC - 1 0 1 0 0 0 ... -> 101000...
+        for (int x : arrCharFreq) {
+            /*
+              To Separate the special edge case of bdddddddddd, bbbbbbbbbbc
+              where the Char Freq Str becomes same for 2 different strings
+              Hence, adding extra space before & after 2 digit freq as salt in the key to make Char Freq Str Key different for each string
+              Before Salt: bdddddddddd -> 010100000000000000000000000  |  bbbbbbbbbbc -> 010100000000000000000000000
+              After  Salt: bdddddddddd -> 010 10 000...  |  bbbbbbbbbbc -> 0 10 1000...
+            */
+            if (x > 9) strKeyCharFreq.append(" ");
+            strKeyCharFreq.append(x);
+            if (x > 9) strKeyCharFreq.append(" ");
+        }
+
+        return strKeyCharFreq.toString();
 
     }
 
