d16e85d120b5f87183a52844907a079b09a06f1c
Harshit Gupta <kohthecodemaster@gmail.com>
2024-03-18 13:36:54 +0530

    37th Commit - LeetCode #: 125. Valid Palindrome - Using Regex & Two Pointers With Constant Space
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S125.java
+++ Java Playground/src/practice/leetcode/S125.java
@@ -8,39 +8,44 @@ public class S125 {
 
     private void major() {
 
-        String s = "A man, a plan, a canal: Panama";
-//        String s = "12";
+//        String s = "A man, a plan, a canal: Panama";
+        String s = ".,";
 
-        boolean result = solutionUsingTwoPointers(s);
+//        boolean result = solutionUsingTwoPointers(s);
 //        boolean result = solutionUsingRegex(s);
+        boolean result = solutionUsingTwoPointersWithConstantSpace(s);
 
         System.out.println("Result: " + result);
 
     }
 
     //  ----------------------------------------------------------------------------------------------------
+
     private boolean solutionUsingTwoPointers(String s) {
         /*
-            Time Complexity:    O(n) - iterates through the input string `s`
-            Space Complexity:   O(1) - Constant Space
+            Time Complexity:    O(n) - Linear time to scan through the input string `s`
+            Space Complexity:   O(n) - Extra n Space for cleanedString
             Approach:           Using Two Pointers
          */
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char c : s.toLowerCase().toCharArray()) if (Character.isLetterOrDigit(c)) stringBuilder.append(c);
+        final String s1 = "hello";
+
+
+        StringBuilder cleanedString = new StringBuilder();
+        for (char c : s.toLowerCase().toCharArray()) if (Character.isLetterOrDigit(c)) cleanedString.append(c);
 
         //  It is an empty string "" after removing non-alphanumeric characters.
         //  Since an empty string reads the same forward and backward, it is a palindrome.
-        if (stringBuilder.length() == 0) return true;
+        if (cleanedString.length() == 0) return true;
 
         boolean isPallindrome = true;
         int left = 0;
-        int right = stringBuilder.length() - 1;
+        int right = cleanedString.length() - 1;
 
         while (left < right) {
 
-            char leftChar = stringBuilder.charAt(left);
-            char rightChar = stringBuilder.charAt(right);
+            char leftChar = cleanedString.charAt(left);
+            char rightChar = cleanedString.charAt(right);
 
             if (leftChar != rightChar) {
                 isPallindrome = false;
@@ -57,16 +62,57 @@ public class S125 {
 
     //  ----------------------------------------------------------------------------------------------------
 
+    private boolean solutionUsingTwoPointersWithConstantSpace(String s) {
+        /*
+            Time Complexity:    O(n) - Linear time to scan through the input string `s`
+            Space Complexity:   O(1) - Constant Space
+            Approach:           Using Two Pointers
+         */
+
+        boolean isPallindrome = true;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) left++;
+            else if (!Character.isLetterOrDigit(rightChar)) right--;
+            else if (leftChar != rightChar) {
+                isPallindrome = false;
+                break;
+            } else {
+                left++;
+                right--;
+            }
+
+        }
+
+        return isPallindrome;
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
     private boolean solutionUsingRegex(String s) {
+        /*
+            Time Complexity:    O(n) - Linear time to scan through the input string `s`
+            Space Complexity:   O(n) - Extra Space used for cleanedString
+            Approach:           Using Regex
+         */
 
-        String s2 = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String cleanedString = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        if (s.length() != s2.length()) return false;
+        if (s.length() != cleanedString.length()) return false;
 
-        for (int i = 0; i < s.length(); i++) if (s.charAt(i) != s2.charAt(s.length() - 1 - i)) return false;
+        for (int i = 0; i < s.length() / 2; i++)
+            if (s.charAt(i) != cleanedString.charAt(s.length() - 1 - i)) return false;
 
         return true;
     }
+
+    //  ----------------------------------------------------------------------------------------------------
 
 }
