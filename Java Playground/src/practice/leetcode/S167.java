ee2d05d3632fa7502ce50aa9f5ab448a053cfc40
Harshit Gupta <kohthecodemaster@gmail.com>
2024-03-20 18:30:49 +0530

    39th Commit - LeetCode #: 167. Two Sum II - Input Array Is Sorted - Using Binary Search
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S167.java
+++ Java Playground/src/practice/leetcode/S167.java
@@ -18,8 +18,9 @@ public class S167 {
         int[] numbers = {12, 13, 23, 28, 43, 44, 59, 60, 61, 68, 70, 86, 88, 92, 124, 125, 136, 168, 173, 173, 180, 199, 212, 221, 227, 230, 277, 282, 306, 314, 316, 321, 325, 328, 336, 337, 363, 365, 368, 370, 370, 371, 375, 384, 387, 394, 400, 404, 414, 422, 422, 427, 430, 435, 457, 493, 506, 527, 531, 538, 541, 546, 568, 583, 585, 587, 650, 652, 677, 691, 730, 737, 740, 751, 755, 764, 778, 783, 785, 789, 794, 803, 809, 815, 847, 858, 863, 863, 874, 887, 896, 916, 920, 926, 927, 930, 933, 957, 981, 997};
         int target = 542;
 
-        int[] resultArr = solutionUsingTwoPointers(numbers, target);
+//        int[] resultArr = solutionUsingTwoPointers(numbers, target);
 //        int[] resultArr = solutionUsingTwoPointersWithFlag(numbers, target);
+        int[] resultArr = solutionUsingBinarySearch(numbers, target);
 
         System.out.println("Result: " + Arrays.toString(resultArr));
 
@@ -94,44 +95,34 @@ public class S167 {
 
     //  ----------------------------------------------------------------------------------------------------
 
-    private int[] solutionUsingBinarySearch(int[] numbers, int target) {
+    public int[] solutionUsingBinarySearch(int[] numbers, int target) {
         /*
-            Time Complexity:    O(n) - Linear time to scan through the input array `numbers`
-            Space Complexity:   O(1) - Constant Space
-            Approach:           Using Binary Search
-         */
+            Time:       O(n log(n))
+            Space:      O(1)
+            Approach:   Using Binary Search
+        */
 
-        int[] indices = new int[2];
-/*
-        int leftPointer = 0;
-        int rightPointer = numbers.length - 1;
+        int[] resultIndices = new int[2];
 
-        while (leftPointer <= rightPointer) {
+        for (int i = 0; i < numbers.length; i++) {
 
-            int mid = (leftPointer + rightPointer) / 2;
-            int num1 = numbers[mid];
+            int num1 = numbers[i];
             int num2 = target - num1;
 
-            if (num2 < 0) {
-                leftPointer++;
-                continue;
+            int startIndex = i + 1;
+            int endIndex = numbers.length - 1;
+
+            int index2 = binarySearch(numbers, startIndex, endIndex, num2);
+
+            if (index2 != -1) {
+                resultIndices[0] = i + 1;
+                resultIndices[1] = index2 + 1;
+                break;
             }
 
-            int indexOfNum2 = binarySearch(numbers, leftPointer, rightPointer, num2);
-*/
+        }
 
-/*
-            if (num1 + num2 == target) {
-                indices[0] = mid;
-                indices[1] = indexOfNum2;
-            } else if () {
-
-            }
-*/
-
-//        }
-
-        return indices;
+        return resultIndices;
 
     }
 
