5ff954bbb44b6b83842c97527c6a4f373a8b7fb6
Harshit Gupta <kohthecodemaster@gmail.com>
2024-04-09 11:13:46 +0530

    56th Commit - LeetCode #: 74. Search a 2D Matrix - Using Two Pointers
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S74.java
+++ Java Playground/src/practice/leetcode/S74.java
@@ -11,7 +11,9 @@ public class S74 {
         int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
         int target = 3;
 
-        boolean result = solutionUsingBinarySearch(matrix, target);
+        boolean result = solutionUsingTwoPointers(matrix, target);
+//        boolean result = solutionUsingBinarySearch(matrix, target);
+//        boolean result = solutionUsingBinarySearch2Pass(matrix, target);
 
         System.out.println("Result: " + result);
 
@@ -21,14 +23,46 @@ public class S74 {
 
     private boolean solutionUsingBinarySearch(int[][] matrix, int target) {
         /*
-            Time Complexity:    O(log(m) + log(n))
-            Space Complexity:   O(n)
+            Time Complexity:    O(log(m * n)) - Binary search over a matrix of dimensions m x n
+            Space Complexity:   O(1) - Constant space, as only a few variables are used
             Approach:           Using Binary Search
-         */
+        */
 
-        //  Handle Edge Cases Efficiently:
-        //  if the target is less than the first element of the matrix or greater than the last element,
-        //  it's guaranteed that the target doesn't exist in the matrix.
+        // Edge Case Check: If the target is out of bounds, return false.
+        if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) return false;
+
+        boolean targetFound = false;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0;
+        int right = (rows * cols) - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / cols][mid % cols];
+
+            if (midVal == target) {
+                targetFound = true;
+                break;
+            } else if (midVal > target) right = mid - 1;
+            else left = mid + 1;
+
+        }
+
+        return targetFound;
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    private boolean solutionUsingBinarySearch2Pass(int[][] matrix, int target) {
+        /*
+            Time Complexity:    O(log(m) + log(n)) - Binary search over a matrix of dimensions m x n
+            Space Complexity:   O(1) - Constant space, as only a few variables are used
+            Approach:           Using Binary Search
+        */
+
+        // Check for Edge Cases: If the target is out of bounds, return false.
         if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) return false;
 
         boolean targetFound;
@@ -58,7 +92,7 @@ public class S74 {
         }
 
         if (rowIndex == -1) rowIndex = right;
-        System.out.println("Left: " + left + " | Right: " + right + " | Row Index: " + rowIndex);
+//        System.out.println("Left: " + left + " | Right: " + right + " | Row Index: " + rowIndex);
 
         return rowIndex;
 
@@ -81,6 +115,34 @@ public class S74 {
         }
 
         return targetIndex;
+
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    private boolean solutionUsingTwoPointers(int[][] matrix, int target) {
+        /*
+            Time Complexity:    O(m + n) - The time complexity is determined by the dimensions of the matrix, m  rows and n columns.
+            Space Complexity:   O(1) - Constant space complexity, as only a few variables are used regardless of the input size.
+            Approach:           Using Two Pointers
+        */
+
+        boolean targetFound = false;
+
+        int left = 0;                       // Start from top row
+        int right = matrix[0].length - 1;   // Start from last column of the first row
+
+        while (left < matrix.length && right >= 0) {
+
+            if (matrix[left][right] == target) {
+                targetFound = true;
+                break;
+            } else if (matrix[left][right] > target) right--;   // Move towards smaller elements
+            else left++;                                        // Move towards larger elements
+
+        }
+
+        return targetFound;
 
     }
 
