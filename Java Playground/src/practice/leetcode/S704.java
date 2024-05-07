8e8f1b9a4fbce25f1e61e765641e744842f08b40
Harshit Gupta <kohthecodemaster@gmail.com>
2024-05-07 17:09:30 +0530

    66th Commit - LeetCode #: 704. Binary Search - Multiple Approaches
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S704.java
+++ Java Playground/src/practice/leetcode/S704.java
@@ -10,12 +10,72 @@ public class S704 {
 
     private void major() {
 
-        int[] nums = {-1, 0, 3, 5, 9, 12};
-        int target = 9;
+//        int[] nums = {-1, 0, 3, 5, 9, 12};
+//        int target = 9;
+        int[] nums = {5};
+        int target = 5;
 
-        int result = solutionUsingBinarySearch(nums, target);
+//        int result = solutionUsingBinarySearch(nums, target);
+
+//        int result = solutionUsingUpperBound(nums, target);
+
+        int result = solutionUsingLowerBound(nums, target);
 
         System.out.println("Result: " + result);
+
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    private int solutionUsingUpperBound(int[] nums, int target) {
+        /*
+            Time Complexity:    O(log n) - Binary Search - Each iteration halves the search space
+            Space Complexity:   O(1) - Constant Space - Excluding output triplets list
+            Approach:           Using Binary Search by finding upper bound
+         */
+
+        int index = -1;
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;    //  Prevent Integer Overflow
+
+            if (nums[mid] <= target) left = mid + 1;
+            else right = mid;
+
+        }
+
+        if (left > 0 && nums[left - 1] == target) index = left - 1;
+        return index;
+
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    private int solutionUsingLowerBound(int[] nums, int target) {
+        /*
+            Time Complexity:    O(log n) - Binary Search - Each iteration halves the search space
+            Space Complexity:   O(1) - Constant Space - Excluding output triplets list
+            Approach:           Using Binary Search by finding lower bound
+         */
+
+        int index = -1;
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;    //  Prevent Integer Overflow
+
+            if (nums[mid] >= target) right = mid;
+            else left = mid + 1;
+
+        }
+
+        if (left < nums.length && nums[left] == target) index = left;
+        return index;
 
     }
 
