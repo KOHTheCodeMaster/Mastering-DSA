725cdfd62421d8531d64ff5f03e3ca166823f657
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-05 09:23:51 +0530

    70th Commit - LeetCode #: 1351. Count Negative Numbers in a Sorted Matrix - Using Two Pointers Optimized
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- Java Playground/src/practice/leetcode/S1351.java
+++ Java Playground/src/practice/leetcode/S1351.java
@@ -12,7 +12,9 @@ public class S1351 {
         int[][] grid = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
 //        int[][] grid = {{3, 2}, {1, 0}};
 
-        int result = solutionUsingLowerBoundBinarySearch(grid);
+//        int result = solutionUsingLowerBoundBinarySearch(grid);
+//        int result = solutionUsingTwoPointers(grid);
+        int result = solutionUsingTwoPointersOptimized(grid);
 
         System.out.println("Result: " + result);
 
@@ -45,6 +47,60 @@ public class S1351 {
 
         return negativeNumCount;
 
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    public int solutionUsingTwoPointers(int[][] grid) {
+        /*
+            Time Complexity:    O(m + n)
+            Space Complexity:   O(1) - Constant Memory
+            Approach:           Two Pointers - currentRow & currentCol
+         */
+
+        int negativeNumCount = 0;
+        int rowCount = grid.length;
+        int colCount = grid[0].length;
+        int row = 0;
+
+        for (int currentCol = colCount - 1; currentCol >= 0; currentCol--) {
+
+            for (int currentRow = row; currentRow < rowCount; currentRow++) {
+                if (grid[currentRow][currentCol] >= 0) row++;
+                else {
+                    negativeNumCount += rowCount - currentRow;
+                    break;
+                }
+            }
+
+        }
+
+        return negativeNumCount;
+
+    }
+
+    //  ----------------------------------------------------------------------------------------------------
+
+    public int solutionUsingTwoPointersOptimized(int[][] grid) {
+        /*
+            Time Complexity:    O(m + n)
+            Space Complexity:   O(1) - Constant Memory
+            Approach:           Two Pointers
+         */
+
+        int count = 0;
+        int cols = grid[0].length;
+        int row = grid.length - 1;
+        int col = 0;
+
+        while (row >= 0 && col < cols) {
+            if (grid[row][col] < 0) {
+                count += cols - col;
+                row--;
+            } else col++;
+        }
+
+        return count;
     }
 
     //  ----------------------------------------------------------------------------------------------------
